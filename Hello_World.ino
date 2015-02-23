5c20c0be641e7157a62d8b59fed548f97619fb1c
sef7 <sef7@aber.ac.uk>
2015-02-23 14:03:26 +0000

    Create Hello_World.ino

---
+++ Hello_World.ino
@@ -0,0 +1,13 @@
+//This is a short piece of arduino code to demonstrate how you write code into GitHub.
+
+String text = "Hello World";
+
+void setup{
+serial.beging(9600);
+
+}
+
+void loop{
+serial.print(text);
+
+}
